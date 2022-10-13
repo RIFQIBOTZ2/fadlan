c99fc1cea7f4f5b0ee0f953b62ecf8d4c3958d44
RIFQIBOTZ2 <104128444+RIFQIBOTZ2@users.noreply.github.com>
2022-10-13 16:47:24 +0700

    Add files via upload

--- jo.js
+++ jo.js
@@ -750,7 +750,7 @@ Silahkan @${m.mentionedJid[0].split`@`[0]} untuk ketik terima/tolak`
             break
 	    case 'donasi': case 'sewabot': case 'sewa': case 'buypremium': case 'donate': {
 	    if (!isCreator) return
-                jobotz.sendMessage(m.chat, { image: { url: 'https://telegra.ph/file/74fd634010128be37972c.jpg' }, caption: `*Hai Kak ${m.pushName}*\n\n Bot Rental Prices\n│➳ 13k Per Group via E-Walet 1 Month\n│➳ 18k via pulsa 1 Month\n\n Premium Price Bot\n│➳ 8k per User 1 bulan\n\nPayment can be via Paypal/link aja/pulsa\n\nFor more details, you can chat with the owner\nhttps://wa.me/6288292024190 (Owner)\n\nDonate For Me : \n\n│➳ Paypal : https://www.paypal.me/Cakhaho\n│➳ Saweria : https://saweria.co/DikaArdnt` }, { quoted: m })
+                jobotz.sendMessage(m.chat, { image: { url: 'https://telegra.ph/file/d37aff85b8757a7f70cb8.jpg' }, caption: `*Hai Kak ${m.pushName}*\n\n Bot Rental Prices\n│➳ 5k` }, { quoted: m })
             }
             break
 
